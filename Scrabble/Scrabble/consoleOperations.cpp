76405d92f3acb89b023012073d290d0e69d59112
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-01-22 17:02:21 +0200

    change clear console

--- Scrabble/Scrabble/consoleOperations.cpp
+++ Scrabble/Scrabble/consoleOperations.cpp
@@ -12,7 +12,8 @@ void printSeparatorLine() {
 }
 
 void clearConsole() {
-	system("cls");
+	cout << "\033[;H"; // Moves cursor to the top left
+	cout << "\033[J"; // Clears the console
 }
 
 // Remove all error flags and characters from the input buffer
@@ -20,12 +21,9 @@ void clearInputBuffer() {
 	// because of using both getline and cin we have to cin.ignore;
 	// cin leaves the newline character in the stream which will be read as input from the getline
 
-	// clears errors flags from the cin
-	cin.clear();
-	// discard unread characters from the input buffer
-	cin.sync();
-	// discard characters from the input buffer
-	cin.ignore();
+	cin.clear();	// clears errors flags from the cin
+	cin.sync();		// discard unread characters from the input buffer
+	cin.ignore();	// discard characters from the input buffer
 }
 
 // Visualize Main Menu in the console
