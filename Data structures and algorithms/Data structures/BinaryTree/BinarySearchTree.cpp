7f57c232f726134613fd8b7966fcb0bad05ad089
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-11-23 22:49:26 +0200

    Full bst

--- Data structures and algorithms/Data structures/BinaryTree/BinarySearchTree.cpp
+++ Data structures and algorithms/Data structures/BinaryTree/BinarySearchTree.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
+#include <queue>
+
 template <typename T>
-struct Node {
+class Node {
 	T value;
-	Node* left;
-	Node* right;
+	Node* left = nullptr;
+	Node* right = nullptr;
 
 	Node(const T& value, Node* left = nullptr, Node* right = nullptr) {
 		this->value = value;
@@ -10,9 +13,40 @@ struct Node {
 		this->right = right;
 	}
 
+	Node(const Node<T>& other) {
+		copyFrom(other);
+	}
+
+	Node& operator=(const Node<T>& other) {
+		if (this != &other) {
+			free();
+			copyFrom(other);
+		}
+
+		return *this;
+	}
+
 	~Node() {
+		free();
+	}
+
+private:
+	void free() {
 		delete left;
 		delete right;
+		left = right = nullptr;
+	}
+
+	void copyFrom(const Node<T>& other) {
+		value = other.value;
+
+		if (other->right) {
+			right = new Node(*other.right);
+		}
+
+		if (other->left) {
+			left = new Node(*other.left);
+		}
 	}
 };
 
@@ -22,21 +56,25 @@ private:
 	Node<T>* root;
 
 	Node<T>* _insert(Node<T>* current, const T& value);
-	bool _contains(Node<T>* current, const T& value) const;
+	bool _contains(const Node<T>* current, const T& value) const;
 	Node<T>* _remove(Node<T>* current, const T& value);
 
+	void _dfs(const Node<T>* current) const;
 public:
 	BinarySearchTree();
-	//BinarySearchTree(const BinarySearchTree<T>&);
-	//BinarySearchTree<T>& operator=(const BinarySearchTree<T>&);
+	BinarySearchTree(const BinarySearchTree<T>& other);
+	BinarySearchTree<T>& operator=(const BinarySearchTree<T>& other);
 	~BinarySearchTree();
 
-	bool contains(const T& value) const;
-	void insert(const T& value);
-	void remove(const T& value);
+	bool contains(const T&) const;
+	void insert(const T&);
+	void remove(const T&);
+
+	void dfs() const;
+	void bfs() const;
 
 private:
-	//void copyFrom(const BinarySearchTree&);
+	void copyFrom(const BinarySearchTree&);
 	void free();
 };
 
@@ -45,31 +83,31 @@ BinarySearchTree<T>::BinarySearchTree() {
 	root = nullptr;
 }
 
-//template <typename T>
-//BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree<T>& other) {
-//	copyFrom(other);
-//}
-//
-//template <typename T>
-//BinarySearchTree<T>& BinarySearchTree<T>::operator=(const BinarySearchTree<T>& other) {
-//	if (this != &other) {
-//		free();
-//		copyFrom(other);
-//	}
-//
-//	return *this;
-//}
+template <typename T>
+BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree<T>& other) {
+	copyFrom(other);
+}
+
+template <typename T>
+BinarySearchTree<T>& BinarySearchTree<T>::operator=(const BinarySearchTree<T>& other) {
+	if (this != &other) {
+		free();
+		copyFrom(other);
+	}
+
+	return *this;
+}
 
 template <typename T>
 BinarySearchTree<T>::~BinarySearchTree() {
 	free();
 }
 
-//template <typename T>
-//void BinarySearchTree<T>::copyFrom(const BinarySearchTree<T>& other) {
-//	// TODO: 
-//}
-//
+template <typename T>
+void BinarySearchTree<T>::copyFrom(const BinarySearchTree<T>& other) {
+	root = new Node<T>(*other.root);
+}
+
 
 template <typename T>
 void BinarySearchTree<T>::free() {
@@ -93,7 +131,7 @@ Node<T>* BinarySearchTree<T>::_insert(Node<T>* current, const T& value) {
 }
 
 template <typename T>
-bool BinarySearchTree<T>::_contains(Node<T>* current, const T& value) const {
+bool BinarySearchTree<T>::_contains(const Node<T>* current, const T& value) const {
 	if (!current) {
 		return false;
 	}
@@ -103,7 +141,7 @@ bool BinarySearchTree<T>::_contains(Node<T>* current, const T& value) const {
 	}
 
 	return current->value > value ? contains(current->left, value) : contains(current->right, value);
-}	
+}
 
 template <typename T>
 Node<T>* BinarySearchTree<T>::_remove(Node<T>* current, const T& value) {
@@ -155,4 +193,38 @@ void BinarySearchTree<T>::insert(const T& value) {
 template <typename T>
 void BinarySearchTree<T>::remove(const T& value) {
 	root = _remove(root, value);
+}
+
+template <typename T>
+void BinarySearchTree<T>::_dfs(const Node<T>* current) const {
+	if (!current) {
+		return;
+	}
+
+	_dfs(current->left);
+	std::cout << current->value << " ";
+	_dfs(current->right);
+}
+
+template <typename T>
+void BinarySearchTree<T>::dfs() const {
+	_dfs(root);
+}
+
+template <typename T>
+void BinarySearchTree<T>::bfs() const {
+	std::queue<Node*> container;
+	container.push(root);
+
+	while (!container.empty()) {
+		Node<T>* current = container.front();
+		container.pop();
+
+		if (current) {
+			std::cout << current->value << " ";
+
+			container.push(current->left);
+			container.push(current->right);
+		}
+	}
 }
