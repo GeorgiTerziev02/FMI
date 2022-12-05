1ef55165659ef6d27fc6da841c16fd2076e7376d
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-12-05 23:32:36 +0200

    add task 2

---
+++ Data structures and algorithms/Exams/Preparation/Exam 4/2020-2021-Task02.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    long long n;
+    std::cin >> n;
+    
+    priority_queue<long long> q;
+    
+    for(size_t i = 0; i < n; i++) {
+        long long input;
+        std::cin >> input;
+        if(!q.empty() && input == -1) {
+            std::cout << q.top() << " ";
+            q.pop();
+        } else if(input != -1) {
+            q.push(input);
+        }
+    }
+    
+    return 0;
+}
