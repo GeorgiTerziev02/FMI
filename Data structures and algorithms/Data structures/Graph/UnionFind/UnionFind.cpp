2e9456087baddc0ff0c68d15248778018998d618
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-01-07 19:21:42 +0200

    add union find

---
+++ Data structures and algorithms/Data structures/Graph/UnionFind/UnionFind.cpp
@@ -0,0 +1,37 @@
+#include "UnionFind.h"
+
+UnionFind::UnionFind(size_t vertices) : parentsContainer(vertices), sizes(vertices) {
+    for (size_t i = 0; i < vertices; i++) {
+        parentsContainer[i] = i;
+        sizes[i] = 1;
+    }
+}
+
+bool UnionFind::areInOneSet(size_t first, size_t second) {
+    return getParent(first) == getParent(second);
+}
+
+size_t UnionFind::getParent(size_t vertex) {
+    if (parentsContainer[vertex] == vertex) {
+        return vertex;
+    }
+    size_t result = getParent(parentsContainer[vertex]);
+    parentsContainer[vertex] = result;
+    return result;
+}
+
+bool UnionFind::unionVertices(size_t first, size_t second) {
+    size_t parentOfFirst = getParent(first);
+    size_t parentOfSecond = getParent(second);
+
+    if(parentOfFirst == parentOfSecond) {
+        return;
+    }
+    
+    if (sizes[parentOfFirst] < sizes[parentOfFirst]) {
+        std::swap(parentOfFirst, parentOfSecond);
+    }
+    // second will have lower size
+    parentsContainer[parentOfSecond] = parentOfFirst;
+    sizes[parentOfFirst] += sizes[parentOfSecond];
+}
